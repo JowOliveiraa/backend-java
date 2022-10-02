5a6f2065ed9bf38f4dc6ba31879013f0760e4377
JowOliveiraa <a42688454@gmail.com>
2022-10-02 19:13:54 -0300

    commi3

--- Orientação a objetos/bytebank/src/CriarConta.java
+++ Orientação a objetos/bytebank/src/CriarConta.java
@@ -5,7 +5,14 @@ public class CriarConta {
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 200;
 
-        System.out.println(primeiraConta.saldo);
+        System.out.println("O saldo da primeira conta é " + primeiraConta.saldo);
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 50;
+
+        System.out.println("O saldo da segunda conta é " + segundaConta.saldo);
+
+
     }
     
 }

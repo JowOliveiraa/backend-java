6f77f54832c568a81c37f3cb7a944e780bea0e50
JowOliveiraa <a42688454@gmail.com>
2022-10-22 01:20:07 -0300

    a

--- Orientação a objetos/bytebank-herdado-conta/src/TesteConta.java
+++ Orientação a objetos/bytebank-herdado-conta/src/TesteConta.java
@@ -1,11 +1,12 @@
 public class TesteConta {
 
+   
     public static void main(String[] args) {
         
         Conta cc = new ContaCorrente(111, 111);
         cc.depositar(100.0);
 
-        ContaPoupanca cp = new ContaPoupanca(222, 222);
+        Conta cp = new ContaPoupanca(222, 222);
         cp.depositar(200.0);
 
         cc.transferir(10.0, cp);

0f79d4cd2cb84c240099f5a55ebd82cfb284ec10
JowOliveiraa <a42688454@gmail.com>
2022-10-31 00:43:22 -0300

    first commit

---
+++ Java Collections- Listas, Sets e Mapas/gerenciador-de-cursos/src/br/com/alura/TestandoListas.java
@@ -0,0 +1,55 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestandoListas {
+
+    public static void main(String[] args) {
+
+        String aula1 = "Conhecendo mais de listas";
+        String aula2 = "Modelando a classe Aula";
+        String aula3 = "Trabalhando com Cursos e Sets";
+
+        ArrayList<String> aulas = new ArrayList<>();
+        aulas.add(aula1);
+        aulas.add(aula2);
+        aulas.add(aula3);
+
+        System.out.println(aulas);
+
+        System.out.println("********************************************");
+
+        aulas.forEach( aula -> System.out.println("Aula: " + aula) );
+
+        System.out.println("********************************************");
+
+        for ( String aula : aulas ) {
+            System.out.println(aula);
+        }
+
+        System.out.println("********************************************");
+
+        System.out.println("A primeira aula é: " + aulas.get(0));
+
+        System.out.println("********************************************");
+
+        for (int i = 0; i < aulas.size(); i++) {
+            System.out.println("Aula: " + aulas.get(i));
+        }
+
+        System.out.println("********************************************");
+
+
+        aulas.add("Aumentando nosso conhecimento");
+        aulas.forEach( aula -> System.out.println(aula));
+
+        System.out.println("********************************************");
+
+        System.out.println("Depois de ordenado em ordem alfabetica: ");
+        Collections.sort(aulas);
+        aulas.forEach( aula -> System.out.println(aula));
+
+
+    }
+}

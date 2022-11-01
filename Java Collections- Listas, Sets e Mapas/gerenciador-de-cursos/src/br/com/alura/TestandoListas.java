f4dd30b9e95d24a9091e897978a48b6f2a86bae8
JowOliveiraa <a42688454@gmail.com>
2022-11-01 20:41:35 -0300

    Update

--- Java Collections- Listas, Sets e Mapas/gerenciador-de-cursos/src/br/com/alura/TestandoListas.java
+++ Java Collections- Listas, Sets e Mapas/gerenciador-de-cursos/src/br/com/alura/TestandoListas.java
@@ -9,7 +9,7 @@ public class TestandoListas {
 
         String aula1 = "Conhecendo mais de listas";
         String aula2 = "Modelando a classe Aula";
-        String aula3 = "Trabalhando com Cursos e Sets";
+        String aula3 = "Trabalhando com Curso e Sets";
 
         ArrayList<String> aulas = new ArrayList<>();
         aulas.add(aula1);

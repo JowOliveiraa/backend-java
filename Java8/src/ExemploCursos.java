f301ba3320216aa6f6e6525f4742cfcb3446f154
Jonatas Oliveira <a42688454@gmail.com>
2022-11-11 23:08:27 -0300

    update

---
+++ Java8/src/ExemploCursos.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.OptionalDouble;
+
+class Curso {
+
+    private String nome;
+    private int alunos;
+
+    public Curso(String nome, int alunos) {
+        this.nome = nome;
+        this.alunos = alunos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAlunos() {
+        return alunos;
+    }
+
+    @Override
+    public String toString() {
+        return this.nome + ", " + this.getAlunos();
+    }
+}
+
+public class ExemploCursos {
+
+    public static void main(String[] args) {
+
+        List<Curso> cursos = new ArrayList<>();
+        cursos.add(new Curso("Python", 45));
+        cursos.add(new Curso("javaScript", 150));
+        cursos.add(new Curso("Java 8", 113));
+        cursos.add(new Curso("C", 55));
+
+         cursos.forEach(System.out::println);
+
+         cursos.sort(Comparator.comparingInt(Curso::getAlunos));
+
+        System.out.println("*****************************************************");
+         cursos.forEach(System.out::println);
+
+        System.out.println("*****************************************************");
+         cursos.stream()
+                 .filter(curso -> curso.getAlunos() >= 100 )
+                 .map(Curso::getNome)
+                 .forEach(System.out::println);
+
+        System.out.println("*****************************************************");
+
+
+         cursos.stream()
+                 .filter(curso-> curso.getAlunos() >= 100)
+                 .map(Curso::getAlunos)
+                 .forEach(System.out::println);
+
+        System.out.println("*****************************************************");
+
+        cursos.stream()
+                .filter(curso -> curso.getAlunos() >= 100)
+                .map(curso -> curso.getNome() + " tem " +curso.getAlunos())
+                .forEach(System.out::println);
+
+        System.out.println("*****************************************************");
+
+        int total = cursos.stream()
+                 .filter(curso -> curso.getAlunos() >= 100)
+                 .mapToInt(Curso::getAlunos)
+                 .sum();
+
+        System.out.println("Total de alunos " + total);
+
+        System.out.println("*****************************************************");
+
+        cursos.stream()
+                .map(Curso::getNome)
+                .forEach(System.out::println);
+
+
+
+
+    }
+
+}
--- Java8/src/OrdenaStrings.java
+++ Java8/src/OrdenaStrings.java
@@ -13,25 +13,25 @@ public class OrdenaStrings {
         palavras.add("Editora Casa do Código");
         palavras.add("Caelum");
 
-        Comparator<String> comparador = new ComparadorPorTamanho();
+//        Comparator<String> comparador = new ComparadorPorTamanho();
 
         //Collections.sort(palavras, comparador);
-        palavras.sort(comparador);
+        palavras.sort((string1, string2) -> Integer.compare(string1.length(), string2.length()));
         System.out.println(palavras);
 
-        palavras.forEach(p -> System.out.println(p));
+        palavras.forEach(System.out::println);
     }
 
 }
 
-class ComparadorPorTamanho implements Comparator<String> {
-
-    @Override
-    public int compare(String s1, String s2) {
-        if (s1.length() < s2.length())
-            return -1;
-        if (s1.length() > s2.length())
-            return 1;
-        return 0;
-    }
-}
+//class ComparadorPorTamanho implements Comparator<String> {
+//
+//    @Override
+//    public int compare(String s1, String s2) {
+//        if (s1.length() < s2.length())
+//            return -1;
+//        if (s1.length() > s2.length())
+//            return 1;
+//        return 0;
+//    }
+//}

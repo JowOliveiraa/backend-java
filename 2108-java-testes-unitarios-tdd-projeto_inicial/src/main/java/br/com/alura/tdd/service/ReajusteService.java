15f4e9526fd611814f3df3ad88abb5e150c864b3
Jonatas Oliveira <a42688454@gmail.com>
2022-11-14 22:43:50 -0300

    update

--- 2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ 2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -7,21 +7,10 @@ import java.math.BigDecimal;
 
 public class ReajusteService {
 
-    BigDecimal reajuste;
     public void recebeReajuste(Funcionario funcionario, Desempenho desempenho) {
 
-        switch (desempenho) {
-            case A_DESEJAR:
-                reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-                break;
-            case BOM:
-                 reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-                break;
-            case OTIMO:
-                 reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-                break;
-        }
-
+        BigDecimal percentual = desempenho.percentualReajuste();
+        BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
         funcionario.reajusteSalario(reajuste);
     }
 }
